df915c89c2e4a3ddf201d3c39a96b7d18cfa95f1
Marcos Ricardo Oliveira <ricardo.oliveira545@gmail.com>
2014-05-12 23:06:14 -0300

    Linhas Principais

---
+++ SimpleCalculator/src/br/simplecalculator/Calcula.java
@@ -0,0 +1,5 @@
+package br.simplecalculator;
+
+public class Calcula {
+
+}
---
+++ SimpleCalculator/src/br/simplecalculator/MainActivity.java
@@ -0,0 +1,46 @@
+package br.simplecalculator;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.os.Build;
+
+
+public class MainActivity extends ActionBarActivity {
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+         }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+    public void realizaOp(char num_ou_simb)
+    {
+    	
+    }
+}
